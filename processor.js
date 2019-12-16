b200068bbf6621c7839e2377378b7baa3c4aeb38
mannya7x <emmanuelestrada94@gmail.com>
2019-12-15 23:44:36 -0600

    more nonsense

--- processor.js
+++ processor.js
@@ -12,16 +12,14 @@ let processor = {
   
     doLoad: function() {
       this.video = document.getElementById("video");
-      this.c1 = document.getElementById("c1");
+      this.c1 = document.getElementById("c3");
       this.ctx1 = this.c1.getContext("2d");
       this.c2 = document.getElementById("c2");
       this.ctx2 = this.c2.getContext("2d");
-      this.c3 = document.getElementById("c3");
-      this.ctx3 = this.c3.getContext("2d");
       let self = this;
       this.video.addEventListener("play", function() {
-          self.width = self.video.videoWidth / 3;
-          self.height = self.video.videoHeight / 3;
+          self.width = self.video.videoWidth / 2;
+          self.height = self.video.videoHeight / 2;
           self.timerCallback();
         }, false);
     },
