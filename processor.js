5e8d189962d777675a5f3b8fa492ce59a312a143
mannya7x <emmanuelestrada94@gmail.com>
2019-12-20 23:21:25 -0600

    swapt for chicken

--- processor.js
+++ processor.js
@@ -12,7 +12,7 @@ let processor = {
   
     doLoad: function() {
       this.video = document.getElementById("video");
-      this.c1 = document.getElementById("c3");
+      this.c1 = document.getElementById("c1");
       this.ctx1 = this.c1.getContext("2d");
       this.c2 = document.getElementById("c2");
       this.ctx2 = this.c2.getContext("2d");
